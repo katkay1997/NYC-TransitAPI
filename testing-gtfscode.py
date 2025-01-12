16f94ab9da5156e9653f91a890292ee33ef883d2
kateramckay <kateradmckay@gmail.com>
2025-01-12 15:24:52 -0500

    The code in this file works

--- testing-gtfscode.py
+++ testing-gtfscode.py
@@ -1,8 +1,15 @@
 from google.transit import gtfs_realtime_pb2
 import requests
 
+
+
 feed = gtfs_realtime_pb2.FeedMessage()
 response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace')
+response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs')
+response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-jz')
+#The output works when I individually type in the endpoints, 
+# but it doesnt work with the Real_Time_Feeds dictionary
+
 feed.ParseFromString(response.content)
 for entity in feed.entity:
   if entity.HasField('trip_update'):
